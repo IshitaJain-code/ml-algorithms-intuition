8e9666b49d22650183e673f5345e3a0c00725a00
IshitaJain-code <ishitajain.1905@gmail.com>
2026-05-10 11:28:50 +0530

    implementation of LR using python

--- linear_regression/implementation.py
+++ linear_regression/implementation.py
@@ -2,6 +2,7 @@ import numpy as np
 
 class SimpleLinearRegression:
     def __init__(self, learning_rate=0.01, n_iterations=1000):
+        
         self.learning_rate = learning_rate
         self.n_iterations = n_iterations
         # slope (m)
